f7bc7026fd3f1c820036a626913532e46b8706e7
Nandeeka Nayak <nandeeka@gmail.com>
2024-08-28 18:19:04 -0500

    Reformat and add conv checking

--- workspace/src/utils.py
+++ workspace/src/utils.py
@@ -30,3 +30,28 @@ def check_matmul(A, B, Z):
                 z_ref += a_val * b_val
 
     print("Result correct?", Z_MN == Z_MN_corr)
+
+def check_conv(I, F, O, step=1):
+    # Note: I, F, and O should be un-partitioned
+    I_BCHW = I.swizzleRanks(rank_ids=["B", "C", "H", "W"])
+    F_MCRS = F.swizzleRanks(rank_ids=["M", "C", "R", "S"])
+    O_BMPQ = O.swizzleRanks(rank_ids=["B", "M", "P", "Q"])
+
+    B, C, H, W = I_BCHW.getShape()
+    M, C, R, S = F_MCRS.getShape()
+    B, M, P, Q = O_BMPQ.getShape()
+
+    O_BMPQ_corr = Tensor(rank_ids=["B", "M", "P", "Q"], name="O")
+    o_b = O_BMPQ_corr.getRoot()
+    i_b = I_BCHW.getRoot()
+    f_m = F_MCRS.getRoot()
+    for b, (o_m, i_c) in o_b << i_b:
+        for m, (o_p, f_c) in o_m << f_m:
+            for c, (i_h, f_r) in i_c & f_c:
+                for r, f_s in f_r:
+                    for s, f_val in f_s:
+                        for p, (o_q, i_w) in o_p << i_h.project(trans_fn=lambda h: -1 / step * r + 1 / step * h, interval=(0, P)).prune(trans_fn=lambda i, c, p: c % 1 == 0):
+                            for q, (o_ref, i_val) in o_q << i_w.project(trans_fn=lambda w: -1 / step * s + 1 / step * w, interval=(0, Q)).prune(trans_fn=lambda i, c, p: c % 1 == 0):
+                                o_ref += i_val * f_val
+
+    print("Result correct?", O_BMPQ == O_BMPQ_corr)
